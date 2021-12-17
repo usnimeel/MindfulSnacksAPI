93466f0d76c2eb6268e7b5871d009868086c7f6c
Minsu Lee <mn394631@dal.ca>
2021-12-17 11:14:33 -0400

    initial setup

---
+++ src/test/java/com/example/MindfulSnakcsAPI/MindfulSnakcsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MindfulSnakcsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfulSnakcsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
